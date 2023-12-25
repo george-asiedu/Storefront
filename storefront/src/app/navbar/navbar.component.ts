e32db5ef5e4c6abe8c524f50301f1f7cb502947d
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-25 18:48:04 +0100

    category-navbar component added to navbar component

--- storefront/src/app/navbar/navbar.component.ts
+++ storefront/src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CategoryNavbarComponent } from '../category-navbar/category-navbar.component';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CategoryNavbarComponent],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
