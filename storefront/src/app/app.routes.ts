8e029f23623bcefb26ade6198301b6acd5591906
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-25 16:19:24 +0100

    navbar routing enabled

--- storefront/src/app/app.routes.ts
+++ storefront/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: NavbarComponent,
+        title: 'Storefront'
+    }
+];
