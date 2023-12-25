5c6f3937d60d01af1d3f923cdaebbbcf2073307d
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2023-12-25 19:35:21 +0100

    router outlet and login route defined

--- storefront/src/app/app.routes.ts
+++ storefront/src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: NavbarComponent,
         title: 'Storefront'
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        title: 'Storefront - Login'
     }
 ];
--- storefront/src/app/category-navbar/category-navbar.component.ts
+++ storefront/src/app/category-navbar/category-navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-category-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './category-navbar.component.html',
   styleUrl: './category-navbar.component.css'
 })
