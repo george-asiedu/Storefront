fead1abc0adcb5407b5bf80cd919ad3f1a11bc64
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-03 13:37:00 +0100

    font-awesome updated

--- DripLuxury-Closet/src/app/app.component.ts
+++ DripLuxury-Closet/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'DripLuxury-Closet';
+  // faCoffee = faCoffee;
 }
