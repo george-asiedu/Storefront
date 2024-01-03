548b4224aa5f23de86f8bef21ee8dee2ff186b46
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-03 09:18:21 +0100

    RouterLink imported to route login and sign up

--- storefront/src/app/navbar/navbar.component.ts
+++ storefront/src/app/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CategoryNavbarComponent } from '../category-navbar/category-navbar.component';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CategoryNavbarComponent],
+  imports: [CategoryNavbarComponent, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
