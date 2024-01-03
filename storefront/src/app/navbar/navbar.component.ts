5b76a7b775de9a503786e4453b2a60ec2bafde03
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-03 09:36:14 +0100

    Update on Navbar component html

--- storefront/src/app/navbar/navbar.component.ts
+++ storefront/src/app/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CategoryNavbarComponent } from '../category-navbar/category-navbar.component';
-import { Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
