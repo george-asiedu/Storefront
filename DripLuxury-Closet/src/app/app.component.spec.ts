2c9795ddbcd56399daa0f0e33a43e89bdfe7a255
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-03 10:55:36 +0100

    packages installed

--- DripLuxury-Closet/src/app/app.component.spec.ts
+++ DripLuxury-Closet/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'storefront' title`, () => {
+  it(`should have the 'DripLuxury-Closet' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('storefront');
+    expect(app.title).toEqual('DripLuxury-Closet');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, storefront');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, DripLuxury-Closet');
   });
 });
--- DripLuxury-Closet/src/app/app.component.ts
+++ DripLuxury-Closet/src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'storefront';
+  title = 'DripLuxury-Closet';
 }
