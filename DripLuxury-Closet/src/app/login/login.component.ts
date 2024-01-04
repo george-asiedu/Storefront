498e09b1da9857f9fa2ab10719f72869d1106068
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-04 16:44:22 +0100

    update on forms

--- DripLuxury-Closet/src/app/login/login.component.ts
+++ DripLuxury-Closet/src/app/login/login.component.ts
@@ -1,15 +1,21 @@
-import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators, EmailValidator } from '@angular/forms';
+import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, RouterLink, CarouselComponent],
+  imports: [ReactiveFormsModule, RouterLink, CarouselComponent, FontAwesomeModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
+    faUser = faUser
+    faLock = faLock
+
     applyLogin = new FormGroup({
       email: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
--- DripLuxury-Closet/src/app/signup/signup.component.ts
+++ DripLuxury-Closet/src/app/signup/signup.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { CarouselComponent } from '../carousel/carousel.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CarouselComponent, ReactiveFormsModule, RouterLink],
+  imports: [CarouselComponent, ReactiveFormsModule, RouterLink, FontAwesomeModule],
   templateUrl: './signup.component.html',
 })
 export class SignupComponent {
+  faUser = faUser
+  faEnvelope = faEnvelope
+  faLock = faLock
+  
   applyLogin = new FormGroup({
     name: new FormControl('', Validators.required),
     email: new FormControl('', Validators.required),
