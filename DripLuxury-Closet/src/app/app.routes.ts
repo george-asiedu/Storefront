20cf15943557b59b088d38e575b2465126d0ecb5
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-04 12:25:45 +0100

    homeNavbar component route defined

--- DripLuxury-Closet/src/app/app.routes.ts
+++ DripLuxury-Closet/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { HomeNavbarComponent } from './home-navbar/home-navbar.component';
 
 export const routes: Routes = [
     {
@@ -12,5 +13,10 @@ export const routes: Routes = [
         path: 'signup',
         component: SignupComponent,
         title: 'DripLuxury - Signup'
+    },
+    {
+        path: '',
+        component: HomeNavbarComponent,
+        title: 'DripLuxury - Home'
     }
 ];
---
+++ DripLuxury-Closet/src/app/home-navbar/home-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-navbar.component.html',
+})
+export class HomeNavbarComponent {
+
+}
