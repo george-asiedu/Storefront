7ecd8c8964ce6d13480f0a345360162dbcd4e99f
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-04 14:38:03 +0100

    logo and search bar

--- DripLuxury-Closet/src/app/home-navbar/home-navbar.component.ts
+++ DripLuxury-Closet/src/app/home-navbar/home-navbar.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FontAwesomeModule],
   templateUrl: './home-navbar.component.html',
 })
 export class HomeNavbarComponent {
-
+  faSearch = faSearch
 }
