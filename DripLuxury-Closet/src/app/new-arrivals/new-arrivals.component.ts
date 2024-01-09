c7077ed1bcf63bd4d7ee01dbcc425f916e416e7e
George Kwabena Asiedu <90930379+george-asiedu@users.noreply.github.com>
2024-01-09 10:47:51 +0100

    cards feature implemented

--- DripLuxury-Closet/src/app/new-arrivals/new-arrivals.component.ts
+++ DripLuxury-Closet/src/app/new-arrivals/new-arrivals.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CardsComponent } from '../cards/cards.component';
 
 @Component({
   selector: 'app-new-arrivals',
   standalone: true,
-  imports: [],
+  imports: [CardsComponent],
   templateUrl: './new-arrivals.component.html',
   styleUrl: './new-arrivals.component.css'
 })
